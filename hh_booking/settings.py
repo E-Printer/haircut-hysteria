f9bce3eb32bd2b875cd1e78581178b4622ade196
Sam Williams <sjwilliams388@gmail.com>
2024-12-17 11:26:39 +0000

    migrated models

--- hh_booking/settings.py
+++ hh_booking/settings.py
@@ -47,9 +47,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
     'cloudinary_storage',
     'cloudinary',
+    'appointments',
 
 ]
 
