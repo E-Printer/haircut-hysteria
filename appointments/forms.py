426c9effd14972640e7a655b8721ec5e0ea21881
Sam Williams <sjwilliams388@gmail.com>
2024-12-19 11:45:11 +0000

    add rank help text to stylist model

--- appointments/forms.py
+++ appointments/forms.py
@@ -1,4 +1,4 @@
-from .models import Booking
+from .models import Booking, Service
 from django import forms
 from django.core.mail import send_mail
 
@@ -12,3 +12,16 @@ class BookingForm(forms.ModelForm):
             "date": forms.DateInput(attrs={"type": "date"}),
             "time": forms.TimeInput(attrs={"type": "time"}),
         }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # Add dynamic attributes to service choices
+
+        self.fields['service'].widget.choices = [
+            (service.id, f"{service.name}") for service in Service.objects.all()
+        ]
+
+        for service in Service.objects.all():
+            self.fields['service'].widget.attrs[f'data-price-{service.id}'] = service.price
+            self.fields['service'].widget.attrs[f'data-duration-{service.id}'] = service.duration
